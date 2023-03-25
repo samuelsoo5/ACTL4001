35a579aafcd44b1a24f3747cc8c9065d929203af
Samuel Soo <zamziteme@gmail.com>
2023-03-25 23:44:09 +1100

    Update

--- 3. Fitting aggregate and region specific poisson distribution.r
+++ 3. Fitting aggregate and region specific poisson distribution.r
@@ -28,6 +28,8 @@ for (i in region_list) {
 
 region_freq[is.na(N), N := 0]
 
+region_freq <- region_freq[-1]
+
 # Performing dispersion tests
 
 dispersion_test <- data.table(Region = "test", pvalue = 0)
@@ -41,13 +43,13 @@ for (i in region_list) {
   
 }
 
-View(dispersion_test)
+#View(dispersion_test)
 
 # Fitting negative binomial models to count data
 
 #pdf("C:/Users/PC/Documents/Uni/4001/Analysis/Nbinom_region_lower.pdf")
 
-nb_params_lower <- data.table(Region = "test", Size = 0, Prob = 0)
+nb_params_lower <- data.table(Region = "test", Size = 0, Mu = 0)
 
 for (i in region_list) {
   
@@ -61,7 +63,7 @@ for (i in region_list) {
   
   plot(hazard_nb)
   
-  nb_params_lower <- rbind(nb_params_lower, data.table(Region = i, Size = hazard_nb$estimate[1], Prob = hazard_nb$estimate[2]))
+  nb_params_lower <- rbind(nb_params_lower, data.table(Region = i, Size = hazard_nb$estimate[1], Mu = hazard_nb$estimate[2]))
 
 }
 
@@ -91,6 +93,8 @@ for (i in region_list) {
 
 region_freq[is.na(N), N := 0]
 
+region_freq <- region_freq[-1]
+
 # Performing dispersion tests
 
 dispersion_test <- data.table(Region = "test", pvalue = 0)
@@ -104,13 +108,13 @@ for (i in region_list) {
   
 }
 
-View(dispersion_test)
+#View(dispersion_test)
 
 # Fitting negative binomial models to count data
 
 #pdf("C:/Users/PC/Documents/Uni/4001/Analysis/Nbinom_region_mid.pdf")
 
-nb_params_mid <- data.table(Region = "test", Size = 0, Prob = 0)
+nb_params_mid <- data.table(Region = "test", Size = 0, Mu = 0)
 
 for (i in region_list) {
   
@@ -124,7 +128,7 @@ for (i in region_list) {
   
   plot(hazard_nb)
   
-  nb_params_mid <- rbind(nb_params_mid, data.table(Region = i, Size = hazard_nb$estimate[1], Prob = hazard_nb$estimate[2]))
+  nb_params_mid <- rbind(nb_params_mid, data.table(Region = i, Size = hazard_nb$estimate[1], Mu = hazard_nb$estimate[2]))
   
 }
 
@@ -158,6 +162,8 @@ for (i in region_list) {
 
 region_freq[is.na(N), N := 0]
 
+region_freq <- region_freq[-1]
+
 # Performing dispersion tests
 
 dispersion_test <- data.table(Region = "test", pvalue = 0)
@@ -171,7 +177,7 @@ for (i in region_list) {
   
 }
 
-View(dispersion_test)
+#View(dispersion_test)
 
 # Fitting poisson models to count data
 
@@ -193,7 +199,7 @@ for (i in region_list) {
     
     plot(hazard_pois)
     
-    pois_params_upper <- rbind(pois_params_upper, data.table(Region = i, Lambda = hazard_nb$estimate[1]))
+    pois_params_upper <- rbind(pois_params_upper, data.table(Region = i, Lambda = hazard_pois$estimate[1]))
     
   }
   
@@ -210,65 +216,52 @@ freak_data <- freak_hazards <- hd[Key %in% c("H2828", "H2827", "W3232",
                                              "F2702", "F2737","F2738", "F2741", "F2746", "F2756",
                                              "H810", "H811", "H812", "H813", "H814")]
 
-freak_data <- freak_data[, .N, .(Region, Year)]
+freak_data <- freak_data[, .N, .(Year)]
 
 # Creating frequency tables
 
-region_freq <- data.table(Region = 0, Year = 0, N = 0)
+region_freq <- data.table(Year = 0, N = 0)
 
-for (i in region_list) {
+region_base <- data.table(Year = seq(1960, 2020))
   
-  region_base <- data.table(Region = rep(i, 61), Year = seq(1960, 2020))
+region_specific <- merge(region_base, freak_data, by = c("Year"), all.x = T)
   
-  region_specific <- merge(region_base, freak_data, by = c("Region", "Year"), all.x = T)
+region_freq <- rbind(region_freq, region_specific)
   
-  region_freq <- rbind(region_freq, region_specific)
-  
-}
-
 region_freq[is.na(N), N := 0]
+
+region_freq <- region_freq[-1]
 
 # Performing dispersion tests
 
-dispersion_test <- data.table(Region = "test", pvalue = 0)
+dispersion_test <- data.table(pvalue = 0)
 
-for (i in region_list) {
+hazard_data_freak <- region_freq[, N]
   
-  hazard_data_freak <- region_freq[Region == i, N]
+dispersion_test <- rbind(dispersion_test, data.table(pvalue = poisdisp.test(hazard_data_freak, alternative = "over", logged = FALSE)[2]))
   
-  dispersion_test <- rbind(dispersion_test, data.table(Region = i,
-                                                       pvalue = poisdisp.test(hazard_data_freak, alternative = "over", logged = FALSE)[2]))
-  
-}
-
-View(dispersion_test)
+#View(dispersion_test)
 
 # Fitting negative binomial models to count data
 
 #pdf("C:/Users/PC/Documents/Uni/4001/Analysis/Nbinom_region_freak.pdf")
 
-nb_params_freak <- data.table(Region = "test", Size = 0, Prob = 0)
+nb_params_freak <- data.table(Region = "test", Size = 0, Mu = 0)
 
-for (i in region_list) {
-  
-  titleplot <- ggplot() + ggtitle(paste0("Region ",i)) + 
-    theme_void() + theme(plot.title = element_text(size = 20, vjust = -35, hjust = 0.5))
-  print(titleplot)
-  
-  
-  
-  hazard_data_freak<- region_freq[Region == i, N]
-    
-  if (sum(hazard_data_freak) > 0) {
-    
-    hazard_nb <- fitdist(hazard_data_freak, "nbinom", method = "mle")
-    
-    plot(hazard_nb)
-    
-    nb_params_freak <- rbind(nb_params_freak, data.table(Region = i, Size = hazard_nb$estimate[1], Prob = hazard_nb$estimate[2]))
+titleplot <- ggplot() + ggtitle(paste0("Region ",i)) + 
+  theme_void() + theme(plot.title = element_text(size = 20, vjust = -35, hjust = 0.5))
+print(titleplot)
 
-  }
+hazard_data_freak<- region_freq[, N]
   
+if (sum(hazard_data_freak) > 0) {
+  
+  hazard_nb <- fitdist(hazard_data_freak, "nbinom", method = "mle")
+  
+  plot(hazard_nb)
+  
+  nb_params_freak <- rbind(nb_params_freak, data.table(Region = i, Size = hazard_nb$estimate[1], Mu = hazard_nb$estimate[2]))
+
 }
 
 nb_params_freak <- nb_params_freak[-1]
