5a8c398f662af7582d4713dd971c3bcdb89a4a10
bronthany <107394369+bronthany@users.noreply.github.com>
2023-03-27 00:08:18 +1100

    Add files via upload

--- EconomicCostModelling.R
+++ EconomicCostModelling.R
@@ -259,10 +259,17 @@ for(s in 1:4){
   
 }
 
-View(scenario_list[[1]])
+# Check how many simulations are over the 10% GDP line
+degcertainty <- scenario_list[[1]][, .(check = sum(I)), Sim_No]
+table(degcertainty$check)
+
 
 # Scenario 1
 ggplot(data = scenario_list[[1]][Sim_No == 167,]) +
+  geom_line(aes(x = Year, y = TotalEconomicCost, color = "Economic Cost")) +
+  geom_line(aes(x = Year, y = TotalPropertyDamage, color = "Property Damage")) +  geom_line(aes(x = Year, y = GDP*100, color = "GDP"))
+
+ggplot(data = aggregate_table[Sim_No == 100,]) +
   geom_line(aes(x = Year, y = TotalEconomicCost, color = "Economic Cost")) +
   geom_line(aes(x = Year, y = TotalPropertyDamage, color = "Property Damage")) +  geom_line(aes(x = Year, y = GDP*100, color = "GDP"))
 
@@ -291,7 +298,14 @@ ggplot(data = scenario_list[[4]]) +
 
 ###########################################
 
-scenario_list_2 <- list()
+scenario_list2 <- list()
+
+# Choose Inflation & Interest rate sim --> nsim[i]
+
+nsim <- seq(2,101,1)
+inflationsim <- c(1.0384, inflation[, c(1, ..nsim[5])][, cuminf := cumprod(.SD[[2]])]$cuminf)
+discountsim <- c(1.0166, rf10[, c(1, ..nsim[5])][, cumdc := cumprod(.SD[[2]])]$cumdc)
+
 
 # Choose Scenario -SSP1 = 1, SSP2 = 2, SSP3 = 3, SSP5 = 4
 for(s in 1:4){
@@ -305,110 +319,166 @@ for(s in 1:4){
   
   
   #total_cost <- data.table(matrix(ncol = 6, nrow = 0, dimnames = list(NULL, c("Year", "GDP", "Total Property Damage", "Total Economic Cost", "Total Injuries", "Total Fatalities"))))
-  total_cost <- rep(0, each = 80)
-  total_damage <- rep(0, each = 80)
-  total_inj <- rep(0, each = 80)
-  total_fat <- rep(0, each = 80)
   
-  # Loop through each region and severity level
-  for(i in 1:length(dt_list_f)){
-    dtf <- dt_list_f[[i]]
-    dts <- dt_list_s[[i]]
-    r <- Regions[i]
-    sev <- Severity[i]
+  
+  # Loop for simulations
+  
+  aggregate_table <- data.table(Sim_No = 0,
+                                Year = 0,
+                                GDP = 0,
+                                TotalPropertyDamage = 0,
+                                TotalEconomicCost = 0,
+                                TotalInj = 0,
+                                TotalFat = 0)
+  
+  for(sim in 1:1000){
+    print(sim)
+    total_temphousing <- rep(0, each = 80)
+    total_LostProd <- trep(0, each = 80)
+    total_FoodCost <- rep(0, each = 80)
+    total_InjuryCost <- rep(0, each = 80)
+    total_Rebuilding <- rep(0, each = 80)
+    total_Replacing <- rep(0, each = 80)
+    total_cost <- rep(0, each = 80)
+    total_damage <- rep(0, each = 80)
+    total_inj <- rep(0, each = 80)
+    total_fat <- rep(0, each = 80)
+    # Loop through each region and severity level
+    for(i in 1:length(dt_list_f)){
+      
+      dtf <- dt_list_f[[i]]
+      dts <- dt_list_s[[i]]
+      r <- Regions[i]
+      sev <- Severity[i]
+      
+      # Multiply Freq and Sev to get total damage for region
+      dtf <- data.table(t(dtf))
+      dts <- data.table(t(dts))
+      
+      
+      # Run for 1000 Simulations (unfinished, only first simulation rn)
+      total <- data.table(Year = seq(2021,2100,1),
+                          GDP = baseline[Regions == r, GDP2020_1000]*g,
+                          Population = baseline[Regions == r, Population]*p,
+                          total = as.numeric(unlist(dtf[, ..sim] * dts[, ..sim])) * 1000 * inflationsim * freqx,
+                          housevalue = baseline[Regions == r, MedianHouse] * inflationsim, 
+                          pphouse = baseline[Regions == r, PpHouse],
+                          injuries = as.numeric(unlist(dtf[, ..sim])) * freqx * injury,
+                          fatalities = as.numeric(unlist(dtf[, ..sim])) * freqx * fatality)
+      total <- total[, no_houses := total/housevalue][, no_pp := no_houses*pphouse][, GDPpercapita := GDP/Population]
+      
+      
+      #### Calculate Region Specific costs ####
+      
+      # Temp Housing
+      total <- total[, TempHousing := no_pp*inflationsim*as.numeric(baseline[Regions == r, TemporaryHousing]*
+                                                                      rec_pro[Severity == sev, 2])*12]
+      
+      # Lost productivity from time taken to recover
+      total <- total[, Lostprod := no_pp*as.numeric(baseline[Regions == r, LabourForce]
+                                                    *baseline[Regions == r, EmploymentRate]
+                                                    *recovery[Severity == sev, 2]
+                                                    *GDPpercapita)*1000]
+      
+      # Lost productivity from Fatalities
+      total <- total[, cumfat :=  frollsum(fatalities, 34, align = "right", fill = NA)]
+      total[1:33, c("cumfat")] <- cumsum(total$fatalities[1:33])
+      total <- total[, FatalityCost := cumfat*GDPpercapita*1000]
+      
+      
+      # Food Cost
+      total <- total[, Foodcost := no_pp*inflationsim*medianfood*as.numeric(recovery[Severity == sev, 2])*365]
+      
+      # Healthcare Cost / Cost of Injury
+      total <- total[, InjuryCost := no_pp*inflationsim*as.numeric(baseline[Regions == r, HealthcareCost])]
+      
+      # Cost of Rebuilding
+      total <- total[, Rebuilding := total*as.numeric(rebuild_multi[Severity == sev, multiplier])]
+      
+      # Cost of replacing household items (40%, can be changed to 75% with 2)
+      total <- total[, Replacing1 := Rebuilding*replacing[1]]
+      total <- total[, Replacing2 := Rebuilding*replacing[2]]
+      
+      # Other costs not considered: 
+      # cost to transportation network because of road and warehouse destruction
+      # cost of lost output of retailers
+      
+      # Total Economic cost for this region and severity level
+      total <- total[, EconomicCost := TempHousing + Lostprod + FatalityCost + Foodcost + InjuryCost +
+                       Rebuilding + Replacing1][, EconomicCost := (round(EconomicCost,0))]
+      
+      total_temphousing <- total_temphousing + total$TempHousing
+      total_LostProd <- total_LostProd + total$Lostprod
+      total_FoodCost <- total_FoodCost + total$Foodcost
+      total_InjuryCost <- total_InjuryCost + total$InjuryCost
+      total_Rebuilding <- total_Rebuilding + total$Rebuilding
+      total_Replacing <- total_Replacing + total$Replacing1
+      total_cost <- total_cost + total$EconomicCost     
+      total_damage <- total_damage + total$total
+      total_inj <- total_inj + total$injuries
+      total_fat <- total_fat + total$fatalities
+      
+      
+    }
     
-    # Multiply Freq and Sev to get total damage for region
-    dtf <- data.table(t(dtf))
-    dts <- data.table(t(dts))
+    aggregate <- data.table(Sim_No = sim,
+                            Year = seq(2021,2100,1),
+                            GDP = totalGDP*g,
+                            TotalPropertyDamage = total_damage,
+                            TotalEconomicCost = total_cost,
+                            TotalInj = total_inj,
+                            TotalFat = total_fat)
+    
+    aggregate_table <- rbind(aggregate_table, aggregate)
+    
+    total_cost <- 0 
     
     
-    # Run for 1000 Simulations (unfinished, only first simulation rn)
-    total <- data.table(Year = seq(2021,2100,1),
-                        GDP = baseline[Regions == r, GDP2020_1000]*g,
-                        Population = baseline[Regions == r, Population]*p,
-                        total = dtf$V1 * dts$V1 * 1000 * inflationsim * freqx,
-                        housevalue = baseline[Regions == r, MedianHouse] * inflationsim, 
-                        pphouse = baseline[Regions == r, PpHouse],
-                        injuries = dtf$V1 * freqx * injury,
-                        fatalities = dtf$V1 * freqx * fatality)
-    total <- total[, no_houses := total/housevalue][, no_pp := no_houses*pphouse][, GDPpercapita := GDP/Population]
-    
-    
-    #### Calculate Region Specific costs ####
-    
-    # Temp Housing
-    total <- total[, TempHousing := no_pp*inflationsim*as.numeric(baseline[Regions == r, TemporaryHousing]*
-                                                                    rec_pro[Severity == sev, 2])*12]
-    
-    # Lost productivity from time taken to recover
-    total <- total[, Lostprod := no_pp*as.numeric(baseline[Regions == r, LabourForce]
-                                                  *baseline[Regions == r, EmploymentRate]
-                                                  *recovery[Severity == sev, 2]
-                                                  *GDPpercapita)*1000]
-    
-    # Lost productivity from Fatalities
-    total <- total[, cumfat :=  frollsum(fatalities, 34, align = "right", fill = NA)]
-    total[1:33, c("cumfat")] <- cumsum(total$fatalities[1:33])
-    total <- total[, FatalityCost := cumfat*GDPpercapita*1000]
-    
-    
-    # Food Cost
-    total <- total[, Foodcost := no_pp*inflationsim*medianfood*as.numeric(recovery[Severity == sev, 2])*365]
-    
-    # Healthcare Cost / Cost of Injury
-    total <- total[, InjuryCost := no_pp*inflationsim*as.numeric(baseline[Regions == r, HealthcareCost])]
-    
-    # Cost of Rebuilding
-    total <- total[, Rebuilding := total*as.numeric(rebuild_multi[Severity == sev, multiplier])]
-    
-    # Cost of replacing household items (40%, can be changed to 75% with 2)
-    total <- total[, Replacing := Rebuilding*replacing[1]]
-    
-    # Other costs not considered: 
-    # cost to transportation network because of road and warehouse destruction
-    # cost of lost output of retailers
-    
-    # Total Economic cost for this region and severity level
-    total <- total[, EconomicCost := TempHousing + Lostprod + FatalityCost + Foodcost + InjuryCost +
-                     Rebuilding + Replacing][, EconomicCost := (round(EconomicCost,0))]
-    
-    total_cost <- total_cost + total$EconomicCost     
-    total_damage <- total_damage + total$total
-    total_inj <- total_inj + total$injuries
-    total_fat <- total_fat + total$fatalities
   }
   
-  
-  aggregate <- data.table(Year = seq(2021,2100,1),
-                          GDP = totalGDP*g,
-                          TotalPropertyDamage = total_damage,
-                          TotalEconomicCost = total_cost,
-                          TotalInj = total_inj,
-                          TotalFat = total_fat)
-  
+  aggregate_table <- aggregate_table[, I := ifelse(GDP*100 < TotalEconomicCost, 1, 0)]
   tablename <- scenarionames[s]
-  scenario_list[[tablename]] <- aggregate
+  scenario_list2[[tablename]] <- aggregate_table
   
 }
 
+# Check how many simulations are over the 10% GDP line
+degcertainty <- scenario_list2[[1]][, .(check = sum(I)), Sim_No]
+table(degcertainty$check)
 
+# aggregate_table <- aggregate_table[, I := ifelse(GDP*100 < TotalEconomicCost, 1, 0)]
+# simcheck <- aggregate_table[, .(check = sum(I)), Sim_No][, check := ifelse(check > 0, 1, 0)]
+# table(simcheck$check)
+
+aggregate_table <- 
 
 # Scenario 1
-ggplot(data = scenario_list[[1]]) +
+ggplot(data = scenario_list2[[1]][Sim_No == 167,]) +
   geom_line(aes(x = Year, y = TotalEconomicCost, color = "Economic Cost")) +
   geom_line(aes(x = Year, y = TotalPropertyDamage, color = "Property Damage")) +  geom_line(aes(x = Year, y = GDP*100, color = "GDP"))
 
-# Scenario 2
-ggplot(data = scenario_list[[2]]) +
+ggplot(data = aggregate_table[Sim_No == 1,]) +
   geom_line(aes(x = Year, y = TotalEconomicCost, color = "Economic Cost")) +
-  geom_line(aes(x = Year, y = TotalPropertyDamage, color = "Property Damage")) # +  geom_line(aes(x = Year, y = GDP*100, color = "GDP"))
+  geom_line(aes(x = Year, y = TotalPropertyDamage, color = "Property Damage")) +  geom_line(aes(x = Year, y = GDP*100, color = "GDP"))
+
+ggplot(data = aggregate_table[Sim_No == 1,]) +
+  geom_bar(aes(x = Year, y = TotalEconomicCost, color = "Economic Cost")) +
+  geom_line(aes(x = Year, y = TotalPropertyDamage, color = "Property Damage")) +  geom_line(aes(x = Year, y = GDP*100, color = "GDP"))
+
+# Scenario 2
+ggplot(data = scenario_list2[[2]]) +
+  geom_line(aes(x = Year, y = TotalEconomicCost, color = "Economic Cost")) +
+  geom_line(aes(x = Year, y = TotalPropertyDamage, color = "Property Damage")) +  geom_line(aes(x = Year, y = GDP*100, color = "GDP"))
 
 # Scenario 3
-ggplot(data = scenario_list[[3]]) +
+ggplot(data = scenario_list2[[3]]) +
   geom_line(aes(x = Year, y = TotalEconomicCost, color = "Economic Cost")) +
-  geom_line(aes(x = Year, y = TotalPropertyDamage, color = "Property Damage")) # +  geom_line(aes(x = Year, y = GDP*100, color = "GDP"))
+  geom_line(aes(x = Year, y = TotalPropertyDamage, color = "Property Damage")) +  geom_line(aes(x = Year, y = GDP*100, color = "GDP"))
 
 # Scenario 4
-ggplot(data = scenario_list[[4]]) +
+ggplot(data = scenario_list2[[4]]) +
   geom_line(aes(x = Year, y = TotalEconomicCost, color = "Economic Cost")) +
-  geom_line(aes(x = Year, y = TotalPropertyDamage, color = "Property Damage")) # +  geom_line(aes(x = Year, y = GDP*100, color = "GDP"))
+  geom_line(aes(x = Year, y = TotalPropertyDamage, color = "Property Damage")) +  geom_line(aes(x = Year, y = GDP*100, color = "GDP"))
+
+
+
